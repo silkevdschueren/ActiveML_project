1f65bfb4c27d3db384f24172271618fcb06e31d4
silkevdschueren <silke.vanderschueren@ugent.be>
2022-11-09 12:55:18 +0100

    Changed code to be able to split in jobs

--- Calculate.py
+++ Calculate.py
@@ -7,13 +7,17 @@ from sklearn import pipeline, preprocessing
 from Methods import *
 
 
-def CalcAllDecisionTree(X, y_true, n_samples=23, batch_size=100, folds=5):
+def CalcActiveML(X, y_true, classifier, active, n_samples=23, batch_size=100, folds=5):
     """
-    Calculate the information needed to plot the learning curves for all active learning 
-    models on a decision tree, and write this information to a txt file.
+    Calculate the information needed to plot the learning curves for a given active learning 
+    model on a given machine learning model, and write this information to a txt file.
 
     :param X: Features of the data.
     :param y_true: True labels of the data.
+    :param classifier: Classifier to use. Options are:
+    RandomForest, LogisticRegression, SVM, DecisionTree.
+    :param active: Active learning method to use. Options are:
+    RandomSampling, UncertaintySampling, QueryByCommittee, CostEmbedding, GreedySampling.
     :param n_samples: Optional parameter giving the number of times a sample should be taken. 
     Default is 23.
     :param batch_size: Optional parameter giving the batch size. Note that 
@@ -23,176 +27,44 @@ def CalcAllDecisionTree(X, y_true, n_samples=23, batch_size=100, folds=5):
     Default is 5.
     """
 
-    clf = SklearnClassifier(DecisionTreeClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    cycle1, accuracies1 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_RandomSampling, n_samples, batch_size, clf)
+    if classifier == "LogisticRegression":
+        clf = SklearnClassifier(LogisticRegression(), classes=np.unique(y_true))
+
+    if classifier == "RandomForest":
+        clf = SklearnClassifier(RandomForestClassifier(criterion = 'entropy'), classes=np.unique(y_true))
+
+    if classifier == "SVM":
+        clf = SklearnClassifier(SVC(kernel = 'rbf', C = 10, probability = True), classes=np.unique(y_true))
+
+    if classifier == "DecisionTree":
+        clf = SklearnClassifier(DecisionTreeClassifier(criterion = 'entropy'), classes=np.unique(y_true))
+
+
+    if active == "RandomSampling":
+        cycles, accuracies = ActiveLearning_CrossValidation(X, y_true, folds, 
+                ActiveLearning_RandomSampling, n_samples, batch_size, clf)
     
-    clf = SklearnClassifier(DecisionTreeClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    _, accuracies2 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_UncertaintySampling, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(DecisionTreeClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    _, accuracies3 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_QueryByCommittee, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(DecisionTreeClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    _, accuracies4 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_CostEmbeddingAL, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(DecisionTreeClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    _, accuracies5 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_GreedySamplingX, n_samples, batch_size, clf)
-    
+    if active == "UncertaintySampling":
+        cycles, accuracies = ActiveLearning_CrossValidation(X, y_true, folds, 
+                ActiveLearning_UncertaintySampling, n_samples, batch_size, clf)
+        
+    if active == "QueryByCommittee":
+        cycles, accuracies = ActiveLearning_CrossValidation(X, y_true, folds, 
+                ActiveLearning_QueryByCommittee, n_samples, batch_size, clf)
+        
+    if active == "CostEmbedding":
+        cycles, accuracies = ActiveLearning_CrossValidation(X, y_true, folds, 
+                ActiveLearning_CostEmbeddingAL, n_samples, batch_size, clf)
+        
+    if active == "GreedySampling":
+        cycles, accuracies = ActiveLearning_CrossValidation(X, y_true, folds, 
+                ActiveLearning_GreedySamplingX, n_samples, batch_size, clf)
+
 
     # Write information to file.
-    f = open(f'dt_{n_samples*batch_size}_{batch_size}.txt', 'w')
-    for i, cycle in enumerate(cycle1):
-        f.write(str(cycle) + ' ' + str(accuracies1[i]) + ' ' + str(accuracies2[i]) + ' ' + 
-                str(accuracies3[i]) + ' ' + str(accuracies4[i]) + ' ' + str(accuracies5[i]) + '\n')
+    f = open(f'{classifier}_{active}_{n_samples}_{batch_size}.txt', 'w')
+    for i, cycle in enumerate(cycles):
+        f.write(str(cycle) + ' ' + str(accuracies[i]) + '\n')
     f.close()
 
     return
-
-
-def CalcAllSVM(X, y_true, n_samples=23, batch_size=100, folds=5):
-    """
-    Calculate the information needed to plot the learning curves for all active learning 
-    models on a support vector machine, and write this information to a txt file.
-
-    :param X: Features of the data.
-    :param y_true: True labels of the data.
-    :param n_samples: Optional parameter giving the number of times a sample should be taken. 
-    Default is 23.
-    :param batch_size: Optional parameter giving the batch size. Note that 
-    n_samples * batch_size can be maximal 4/5*(total size dataset) - batch_size 
-    if there is 5 fold cross validation. Default value 100.
-    :param folds: Optional parameter giving the number of partitions for cross validation.
-    Default is 5.
-    """
-
-    clf = SklearnClassifier(SVC(kernel = 'rbf', C = 10, probability = True), 
-            classes=np.unique(y_true))
-    cycle1, accuracies1 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_RandomSampling, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(SVC(kernel = 'rbf', C = 10, probability = True), 
-            classes=np.unique(y_true))
-    _, accuracies2 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_UncertaintySampling, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(SVC(kernel = 'rbf', C = 10, probability = True), 
-            classes=np.unique(y_true))
-    _, accuracies3 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_QueryByCommittee, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(SVC(kernel = 'rbf', C = 10, probability = True), 
-            classes=np.unique(y_true))
-    _, accuracies4 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_CostEmbeddingAL, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(SVC(kernel = 'rbf', C = 10, probability = True), 
-            classes=np.unique(y_true))
-    _, accuracies5 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_GreedySamplingX, n_samples, batch_size, clf)
-
-    # Write information to file
-    f = open(f'svm_{n_samples*batch_size}_{batch_size}.txt', 'w')
-    for i, cycle in enumerate(cycle1):
-        f.write(str(cycle) + ' ' + str(accuracies1[i]) + ' ' + str(accuracies2[i]) + ' ' + 
-                str(accuracies3[i]) + ' ' + str(accuracies4[i]) + ' ' + str(accuracies5[i]) + '\n')
-    f.close()
-    
-    return
-
-
-def CalcAllLogisticRegression(X, y_true, n_samples=23, batch_size=100, folds=5):
-    """
-    Calculate the information needed to plot the learning curves for all active learning 
-    models on logistic regression, and write this information to a txt file.
-
-    :param X: Features of the data.
-    :param y_true: True labels of the data.
-    :param n_samples: Optional parameter giving the number of times a sample should be taken. 
-    Default is 23.
-    :param batch_size: Optional parameter giving the batch size. Note that 
-    n_samples * batch_size can be maximal 4/5*(total size dataset) - batch_size 
-    if there is 5 fold cross validation. Default value 100.
-    :param folds: Optional parameter giving the number of partitions for cross validation.
-    Default is 5.
-    """
-
-    clf = SklearnClassifier(LogisticRegression(), classes=np.unique(y_true))
-    cycle1, accuracies1 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_RandomSampling, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(LogisticRegression(), classes=np.unique(y_true))
-    _, accuracies2 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_UncertaintySampling, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(LogisticRegression(), classes=np.unique(y_true))
-    _, accuracies3 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_QueryByCommittee, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(LogisticRegression(), classes=np.unique(y_true))
-    _, accuracies4 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_CostEmbeddingAL, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(LogisticRegression(), classes=np.unique(y_true))
-    _, accuracies5 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_GreedySamplingX, n_samples, batch_size, clf)
-
-    # Write information to file
-    f = open(f'lr_{n_samples*batch_size}_{batch_size}.txt', 'w')
-    for i, cycle in enumerate(cycle1):
-        f.write(str(cycle) + ' ' + str(accuracies1[i]) + ' ' + str(accuracies2[i]) + ' ' + 
-                str(accuracies3[i]) + ' ' + str(accuracies4[i]) + ' ' + str(accuracies5[i]) + '\n')
-    f.close()
-
-    return
-
-
-def CalcAllRandomForest(X, y_true, n_samples=23, batch_size=100, folds=5):
-    """
-    Calculate the information needed to plot the learning curves for all active learning 
-    models on a random forest classifier, and write this information to a txt file.
-
-    :param X: Features of the data.
-    :param y_true: True labels of the data.
-    :param n_samples: Optional parameter giving the number of times a sample should be taken. 
-    Default is 23.
-    :param batch_size: Optional parameter giving the batch size. Note that 
-    n_samples * batch_size can be maximal 4/5*(total size dataset) - batch_size 
-    if there is 5 fold cross validation. Default value 100.
-    :param folds: Optional parameter giving the number of partitions for cross validation.
-    Default is 5.
-    """
-
-    clf = SklearnClassifier(RandomForestClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    cycle1, accuracies1 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_RandomSampling, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(RandomForestClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    _, accuracies2 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_UncertaintySampling, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(RandomForestClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    _, accuracies3 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_QueryByCommittee, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(RandomForestClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    _, accuracies4 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_CostEmbeddingAL, n_samples, batch_size, clf)
-    
-    clf = SklearnClassifier(RandomForestClassifier(criterion = 'entropy'), classes=np.unique(y_true))
-    _, accuracies5 = ActiveLearning_CrossValidation(X, y_true, folds, 
-            ActiveLearning_GreedySamplingX, n_samples, batch_size, clf)
-
-    # Write information to file
-    f = open(f'rf_{n_samples*batch_size}_{batch_size}.txt', 'w')
-    for i, cycle in enumerate(cycle1):
-        f.write(str(cycle) + ' ' + str(accuracies1[i]) + ' ' + str(accuracies2[i]) + ' ' + 
-                str(accuracies3[i]) + ' ' + str(accuracies4[i]) + ' ' + str(accuracies5[i]) + '\n')
-    f.close()
-
-    return
-       
--- CalculateAll.py
+++ CalculateAll.py
@@ -1,31 +1,28 @@
 from Calculate import *
-
+import sys
 
 def main():
 
-    print("Reading data...")
-    #read in dataframe
+    # Arguments given to the program in command line
+    classifier, active, n_samples, batch_size = sys.argv[1:]
+    n_samples = int(n_samples)
+    batch_size = int(batch_size)
+
+    # Read in dataframe
     data = pd.read_csv('dataset_1.csv')
     
-    #define the target and categorize it in numbers
+    # Define the target and categorize it in numbers
     target = 'PROFILE'
     profile_options = np.unique(data['PROFILE'])
     for index, profile in enumerate(profile_options):
         data.loc[data['PROFILE'] == profile,'PROFILE'] = index
     
-    #then get your input features and labels
+    # Then get your input features and labels
     X = np.array(data.drop(target, axis=1).values.tolist())
     y_true = np.array(data[target]).astype('int')
     
-    
-    print("Calculating Random Forest...")
-    CalcAllRandomForest(X, y_true, n_samples=3, batch_size=100)
-    print("Calculating Logistic Regression...")
-    CalcAllLogisticRegression(X, y_true, n_samples=3, batch_size=100)
-    print("Calculating SVM...")
-    CalcAllSVM(X, y_true, n_samples=3, batch_size=100)
-    print("Calculating Decision Tree...")
-    CalcAllDecisionTree(X, y_true, n_samples=3, batch_size=100)
+
+    CalcActiveML(X, y_true, classifier, active, n_samples, batch_size)
 
     return
 
